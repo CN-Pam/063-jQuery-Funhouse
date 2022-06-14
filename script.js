f6127f71f1a96eeab41343806b4faf202d375084
pamela-martinez <72035155+pamela-martinez@users.noreply.github.com>
2022-06-14 12:30:50 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".garage-door-opener").click(function() {
+ $(".garage-door").slideToggle ();   
+});
+
+$(".pokeball  ").click(function() {
+  $(".pokeball").fadeOut();  
+});
+
+$(".light-switch").click(function() {
+   $(".light-bulb").fadeToggle();
+
+});
